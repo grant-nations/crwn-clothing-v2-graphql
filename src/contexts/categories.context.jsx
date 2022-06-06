69b49c20ed6f4dd5d458fbe6a77f63a39f223016
GRANT NATIONS <u1247784@umail.utah.edu>
2022-06-06 08:49:02 -0600

    set up graphql to query products

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,27 +1,48 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import {createContext, useState, useEffect} from 'react';
+import {getCategoriesAndDocuments} from '../utils/firebase/firebase.utils';
+import {gql, useQuery} from "@apollo/client";
 
 export const CategoriesContext = createContext({
-  categoriesMap: {},
+    categoriesMap: {},
 });
 
-export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+const COLLECTIONS = gql`
+    query {
+        collections{
+        id
+        title
+        items {
+            id
+            name
+            price
+            imageUrl
+            }
+        }
+    }
+  `
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+export const CategoriesProvider = ({children}) => {
 
-    getCategoriesMap();
-  }, []);
+    const {loading, error, data} = useQuery(COLLECTIONS);
+    const [categoriesMap, setCategoriesMap] = useState({});
 
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
+    useEffect(() => {
+        if (data) {
+            const {collections} = data;
+            const collectionsMap = collections.reduce((acc, collection) => {
+                const {title, items} = collection;
+                acc[title.toLowerCase()] = items;
+                return acc;
+            }, {})
+
+            setCategoriesMap(collectionsMap);
+        }
+    }, [data])
+
+    const value = {categoriesMap, loading};
+    return (
+        <CategoriesContext.Provider value={value}>
+            {children}
+        </CategoriesContext.Provider>
+    );
 };
--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,21 +1,23 @@
-import { useContext, Fragment } from 'react';
+import {useContext, Fragment} from 'react';
+import Spinner from "../../components/spinner/spinner.component";
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import {CategoriesContext} from '../../contexts/categories.context';
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 const CategoriesPreview = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+    const {categoriesMap, loading} = useContext(CategoriesContext);
 
-  return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            {loading ? <Spinner/> : Object.keys(categoriesMap).map((title) => {
+                const products = categoriesMap[title];
+                return (
+                    <CategoryPreview key={title} title={title} products={products}/>
+                );
+            })}
+
+        </Fragment>
+    );
 };
 
 export default CategoriesPreview;
